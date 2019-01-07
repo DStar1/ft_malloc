1125502c452e55eb2dbd5a6e440449fa05f8e0c8
dstar1 <harrisonfsmith95@gmail.com>
2019-01-06 18:12:10 -0800

    some ideas of how to write the malloc function and will be designing  the allocation function

--- includes/ft_malloc.h
+++ includes/ft_malloc.h
@@ -2,5 +2,20 @@
 # define FT_MALLOC_H
 
 # include <stdio.h>
+# include "libft.h"
+# include <sys/mman.h>
+# include <errno.h>
+
+
+# define SMALL_PAGE_SIZE 8
+# define SMALL_SIZE (8 * 4096)
+# define MED_PAGE_SIZE 32
+# define MED_SIZE (32 * 4096)
+
+typedef struct		s_link {
+	struct s_link	*end;
+	struct s_link	*next;
+}					t_link;
+
 
 #endif 
--- srcs/main.c
+++ srcs/main.c
@@ -1,7 +1,40 @@
 #include "ft_malloc.h"
 
+
+/*
+** (IDEA for alloc_small_med() function)
+** allocate using mmap and then create new link
+** 
+** 
+** 
+*/
+
+// static void		*alloc_small_med(t_link *start, long size, long max)
+// {
+
+// }
+
+/*
+** (IDEA for malloc() function)
+** t_link *node
+** if size <= SMALL_SIZE {ret(alloc(small))}
+** if size <= MED_SIZE {ret(alloc(med))}
+** node = aloc(large)
+** iterate though node to get to NULL
+** node->next = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+** if this node == NULL {errno = ENOMEM; return(NULL)}
+** node->next->end = add size
+** node->next->next = NULL
+** ret(node->next+1)
+*/
+
+// void	malloc(size_t size)
+// {
+
+// }
+
 int		main(int argc, char *argv[])
 {
-	printf("Project %s successfully created! \n", argv[0]);
+	ft_printf("Project %s successfully created! \n", argv[0]);
 	return (argc);
 }
